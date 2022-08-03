f5170f60fc3e12701d8a2a3dafe5836a4bfd53a1
Vadim Vladimirov <kurtonchik@gmail.com>
2022-08-03 22:32:39 +0300

    end part

--- src/components/PostContainer.tsx
+++ src/components/PostContainer.tsx
@@ -1,16 +1,39 @@
 import React from "react";
+import { IPost } from "../models/IPost";
 import { postApi } from "../services/PostService";
 import PostItem from "./PostItem";
 
 const PostContainer = () => {
-  const { data: posts, error, isLoading } = postApi.useFetchAllPostsQuery(7);
+  const { data: posts, error, isLoading } = postApi.useFetchAllPostsQuery(15);
+  const [createPost, {}] = postApi.useCreatePostMutation();
+  const [udpatePost, {}] = postApi.useUpdatePostMutation();
+  const [deletePost, {}] = postApi.useDeletePostMutation();
+
+  const HandleCreate = async () => {
+    const title = prompt();
+    await createPost({ title, body: title } as IPost);
+  };
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post);
+  };
+
+  const handleUpdate = (post: IPost) => {
+    udpatePost(post);
+  };
   return (
     <div>
+      <button onClick={HandleCreate}>Add new post</button>
       {isLoading && <h1>Loading</h1>}
       {error && <h1>An error occurred while loading </h1>}
       <ul>
         {posts?.map((post) => (
-          <PostItem key={post.id} post={post} />
+          <PostItem
+            remove={handleRemove}
+            update={handleUpdate}
+            key={post.id}
+            post={post}
+          />
         ))}
       </ul>
     </div>
